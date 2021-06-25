70ffdf2d332f61cd39ef46697787388891034dc1
Nick <tokarew2001@gmail.com>
2021-06-25 15:11:31 +0300

    completion of the first block

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 60px auto;
-  width: 400px;
+  width: 300px;
 }
 </style>
--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -2,42 +2,53 @@
   <div class="hello">
 
     <div class="step" v-show="step === 1">
-         <div class="form-group" :class="{ 'form-group--error': $v.surname.$error }">
-             <label class="form__label">Фамилия<span class="error" v-if="!$v.surname.required">*</span><br>
-             <input class="form__input" v-model.trim="surname" @input="setSurname($event.target.value)" type="text"/></label>
+         <div class="form-group">
+             <label class="form__label">Фамилия*<br>
+             <input class="form__input" v-model.trim="surname" @input="setSurname($event.target.value)" type="text"
+             maxlength="20" :class="{ 'form-group--error': $v.surname.$error }"/></label>
          </div>
+        <span class="error" v-if="!$v.surname.required"> Поле обязательно для заполнения</span>
           <span class="error" v-if="!$v.surname.minLength"> Не менее {{$v.surname.$params.minLength.min}} букв.</span>
-          <span class="error" v-if="!$v.surname.alpha"> Недопустимые символы</span>
+          <span class="error" v-else-if="!$v.surname.alpha"> Недопустимые символы</span>
 
-         <div class="form-group" :class="{ 'form-group--error': $v.name.$error }">
-            <label class="form__label">Имя<span class="error" v-if="!$v.name.required">*</span><br>
-            <input class="form__input" v-model.trim="name" @input="setName($event.target.value)" type="text"/></label>
+         <div class="form-group">
+            <label class="form__label">Имя*<br>
+            <input class="form__input" v-model.trim="name" @input="setName($event.target.value)" type="text"
+            maxlength="20" :class="{ 'form-group--error': $v.name.$error }"/></label>
           </div>
+        <span class="error" v-if="!$v.name.required"> Поле обязательно для заполнения</span>
           <span class="error" v-if="!$v.name.minLength"> Не менее {{$v.name.$params.minLength.min}} букв.</span>
-          <span class="error" v-if="!$v.name.alpha"> Недопустимые символы</span>
+          <span class="error" v-else-if="!$v.name.alpha"> Недопустимые символы</span>
 
-           <div class="form-group" :class="{ 'form-group--error': $v.patronymic.$error }">
+           <div class="form-group">
              <label class="form__label">Отчество<br>
-             <input class="form__input" v-model.trim="patronymic" @input="setPatronymic($event.target.value)" type="text"/></label>
+             <input class="form__input" v-model.trim="patronymic" @input="setPatronymic($event.target.value)" type="text"
+             maxlength="20"/></label>
            </div>
            <span class="error" v-if="!$v.patronymic.minLength"> Не менее {{$v.patronymic.$params.minLength.min}} букв.</span>
-           <span class="error" v-if="!$v.patronymic.alpha"> Недопустимые символы</span>
+           <span class="error" v-else-if="!$v.patronymic.alpha"> Недопустимые символы</span>
+
+        <div class="form-group">
+            <label class="form__label">Дата рождения*<br>
+                <input class="form__input" v-model.trim="birthdate" @input="setBirthdate($event.target.value)" type="date"
+                       :class="{ 'form-group--error': $v.birthdate.$error }"/></label>
+        </div>
+        <span class="error" v-if="!$v.birthdate.required"> Поле обязательно для заполнения</span>
 
          <button @click="nextStep" type="button" class="btn">Следующий шаг</button>
   </div>
 
 
     <div class="step" v-show="step === 2">
-         <div class="form-group" :class="{ 'form-group--error': $v.birthdate.$error }">
-             <label class="form__label">Дата рождения<span class="error" v-if="!$v.birthdate.required">*</span><br>
-             <input class="form__input" v-model.trim="birthdate" @input="setBirthdate($event.target.value)" type="date"/></label>
-         </div>
 
-          <div class="form-group" :class="{ 'form-group--error': $v.phoneNumber.$error }">
-              <label class="form__label">Номер телефона<span class="error" v-if="!$v.phoneNumber.required">*</span><br>
+
+          <div class="form-group">
+              <label class="form__label">Номер телефона*<br>
               <input class="form__input" v-model.trim="phoneNumber" @input="setPhoneNumber($event.target.value)" type="tel"
-             placeholder="7-xxx-xxx-xxxx" maxlength="14" minlength="11"/></label>
+                     placeholder="7-xxx-xxx-xxxx" maxlength="14" minlength="11"
+                     :class="{ 'form-group--error': $v.phoneNumber.$error }"/></label>
           </div>
+        <span class="error" v-if="!$v.phoneNumber.required"> Поле обязательно для заполнения</span>
           <span class="error" v-if="!$v.phoneNumber.phone"> Неверно набран номер</span>
 
           <div class="form-group">
@@ -48,14 +59,16 @@
           </div>
 
           <div class="form-group">
-              <label class="form__label">Группа клиентов<span class="error" v-if="!$v.typeOfClient.required">*</span>
-                 <select multiple="multiple" size="3" class="type-of-client" v-model.trim="typeOfClient" @select="setTypeOfClient($event.target.value)">
+              <label class="form__label">Группа клиентов*
+                 <select multiple="multiple" size="3" class="type-of-client" v-model.trim="typeOfClient"
+                         @select="setTypeOfClient($event.target.value)"  :class="{ 'form-group--error': $v.typeOfClient.$error }">
                      <option > VIP</option>
                      <option> Проблемные</option>
                      <option> ОМС</option>
                  </select>
               </label>
           </div>
+        <span class="error" v-if="!$v.typeOfClient.required"> Поле обязательно для заполнения</span>
 
          <div class="form-group">
              <label>Лечащий врач
@@ -73,11 +86,63 @@
               </label>
           </div>
 
-          <button @click="backStep" type="button" class="btn">Назад</button>
-          <button @click="nextStep" type="button" class="btn">Следующий шаг</button>
+            <div class="flex-btn">
+                  <button @click="backStep" type="button" class="btn">Назад</button>
+                   <button @click="nextStep" type="button" class="btn">Следующий шаг</button>
+            </div>
+
      </div>
 
+      <div class="step" v-show="step === 3">
+          <div class="form-group">
+              <label class="form__label">Индекс<br>
+                  <input class="form__input" v-model.trim="regionIndex" @input="setRegionIndex($event.target.value)" type="text"
+                  placeholder="xxxxxx" maxlength="6"/></label>
+          </div>
+          <span class="error" v-if="!$v.regionIndex.indexRegions"> Недопустимые символы</span>
 
+          <div class="form-group">
+              <label class="form__label">Страна<br>
+                  <input class="form__input" v-model.trim="country" type="text"
+                  maxlength="20"/></label>
+          </div>
+          <span class="error" v-if="!$v.country.alpha"> Недопустимые символы</span>
+
+          <div class="form-group">
+              <label class="form__label">Область<br>
+                  <input class="form__input" v-model.trim="region" type="text"
+                  maxlength="25"/></label>
+          </div>
+          <span class="error" v-if="!$v.region.alpha"> Недопустимые символы</span>
+
+          <div class="form-group">
+              <label class="form__label">Город*<br>
+                  <input class="form__input" v-model.trim="city" @input="setCity($event.target.value)" type="text"
+                  maxlength="30"  :class="{ 'form-group--error': $v.typeOfClient.$error }"/></label>
+          </div>
+          <span class="error" v-if="!$v.city.required"> Поле обязательно для заполнения</span>
+          <span class="error" v-if="!$v.city.minLength"> Не менее {{$v.city.$params.minLength.min}} букв.</span>
+          <span class="error" v-else-if="!$v.city.alpha"> Недопустимые символы</span>
+
+          <div class="form-group">
+              <label class="form__label">Улица<br>
+                  <input class="form__input" v-model.trim="surname" @input="setSurname($event.target.value)" type="text"
+                  maxlength="30"/></label>
+          </div>
+          <span class="error" v-if="!$v.surname.minLength"> Не менее {{$v.surname.$params.minLength.min}} букв.</span>
+
+          <div class="form-group">
+              <label class="form__label">Дом<br>
+                  <input class="form__input" v-model.trim="surname" @input="setSurname($event.target.value)" type="text"
+                  maxlength="30"/></label>
+          </div>
+
+          <div class="flex-btn">
+              <button @click="backStep" type="button" class="btn">Назад</button>
+              <button @click="nextStep" type="button" class="btn">Следующий шаг</button>
+          </div>
+
+      </div>
 
 
   </div>
@@ -85,8 +150,9 @@
 
 <script>
   import { required, minLength, helpers } from 'vuelidate/lib/validators'
-const alpha = helpers.regex('alpha', /^[ЁА-Яёа-я]*$/)
-  const phone = helpers.regex('phone', /^\(?([7]{1})\)?[-. ]?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)
+    const alpha = helpers.regex('alpha', /^[ЁА-Яёа-я]*$/)
+    const phone = helpers.regex('phone', /^\(?([7]{1})\)?[-. ]?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)
+    let indexRegions = helpers.regex('indexRegions', /^[0-9]*$/)
 
 export default {
   name: 'HelloWorld',
@@ -99,6 +165,10 @@ export default {
       birthdate: '',
       phoneNumber: '',
       typeOfClient: [ ],
+      regionIndex: '',
+      country: '',
+      region: '',
+      city: '',
     }
   },
   validations: {
@@ -113,7 +183,6 @@ export default {
       alpha
     },
     patronymic:{
-      required,
       minLength: minLength(6),
       alpha
     },
@@ -126,7 +195,22 @@ export default {
     },
     typeOfClient: {
       required
-    }
+    },
+      regionIndex: {
+        required,
+        indexRegions
+      },
+      country: {
+        alpha
+      },
+      region: {
+        alpha
+      },
+      city: {
+        alpha,
+        required,
+          minLength: minLength(3)
+      }
   },
 
   methods: {
@@ -154,6 +238,14 @@ export default {
     this.typeOfClient = value
     this.$v.typeOfClient.$touch()
   },
+      setRegionIndex(value) {
+        this.regionIndex = value
+          this.$v.regionIndex.$touch()
+      },
+      setCity(value) {
+          this.city = value
+          this.$v.city.$touch()
+      },
     nextStep() {
       this.step++
     },
@@ -169,15 +261,17 @@ export default {
 .form-group{
   margin-top: 20px;
 }
-input {
+.form__input{
   border: 1px solid silver;
   border-radius: 4px;
   background: white;
   padding: 5px 10px;
   font-size: 17px;
+    width: 92%;
 }
 .error {
   color: red;
+    font-size: 14px;
 }
   .type-of-client{
     display: block;
@@ -203,9 +297,16 @@ input {
   border-radius: 6px;
   background: linear-gradient(#F3AE0F, #E38916) #E38916;
   transition: 0.2s;
-  margin-right: 40px;
   margin-top: 30px;
 }
 .btn:hover { background: linear-gradient(#f5ae00, #f59500) #f5ae00; }
 .btn:active { background: linear-gradient(#f59500, #f5ae00) #f59500; }
+
+    .form-group--error{
+        border-color: red;
+    }
+    .flex-btn{
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
